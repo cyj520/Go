1016800fad4abfd245ddd5a32bbb6e5ad7e74807
cyj <1010069793@qq.com>
2024-06-20 16:37:03 +0800

    message

--- 00-基础语法/codes/demo_1.go
+++ 00-基础语法/codes/demo_1.go
@@ -15,6 +15,6 @@ func main() {
 	const name_1, name_2 string = "Tom", "Jay"
 	fmt.Println(name_1, name_2)
 
-	const name_3, age_1 = "Tom", 30
+	const name_3, age_1 = "Tom", 32
 	fmt.Println(name_3, age_1)
 }
